5a55148d7590a8bc91ca91a00cd614b8726424e6
Mark West <markwest1972@users.noreply.github.com>
2017-01-08 09:25:36 +0100

    Create s3-upload-file.js

---
+++ s3-upload/s3-upload-file.js
@@ -0,0 +1,44 @@
+//
+// Uploads pictures to an upload directory in your s3 bucket.
+//
+// This module requires two command-line arguments:
+// 1. Destination bucket
+// 2. File to upload
+//
+// Example : "node s3-upload-image-file.js <s3-bucket-name> <path-to-image>"
+//
+// This module also requires config.json file with AWS authorisation and locale parameters.
+//
+
+// Load the SDK for JavaScript
+var AWS = require('aws-sdk');
+var path = require('path');
+var fs = require('fs');
+
+// Load configration file
+AWS.config.loadFromPath('./config.json');
+
+// Create S3 service object
+s3 = new AWS.S3({apiVersion: '2006-03-01'});
+
+// Create stream for upload
+var file = process.argv[3];
+var fileStream = fs.createReadStream(file);
+fileStream.on('error', function(err) {
+  console.log('File Error', err);
+});
+
+// Populate upload parameters
+var uploadParams = {Bucket: process.argv[2], Key: '', Body: ''};
+uploadParams.Key = 'upload/'+path.basename(file);
+uploadParams.Body = fileStream;
+uploadParams.ACL = 'public-read';
+
+// call S3 to retrieve upload file to specified bucket
+s3.upload (uploadParams, function (err, data) {
+  if (err) {
+    console.log('Upload Error', err);
+  } if (data) {
+    console.log('Upload Success', '[' + data.Location + ']');
+  }
+});
