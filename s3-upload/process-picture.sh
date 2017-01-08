41508dcf0a64f2f0de55af06e1d9810801fbce28
Mark West <markwest1972@users.noreply.github.com>
2017-01-08 09:55:16 +0100

    Update process-picture.sh

--- s3-upload/process-picture.sh
+++ s3-upload/process-picture.sh
@@ -4,7 +4,7 @@ echo starting s3-upload.sh
 
 echo uploading file $1
 
-node s3-upload-file.js smart-security-camera-upload  $1
+node s3-upload-file.js your-bucket-name  $1
 
 echo deleting file $1
 
