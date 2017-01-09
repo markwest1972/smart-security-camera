4384e89f037eb0ad3e419c8a897cdf093d56cf15
Mark West <markwest1972@users.noreply.github.com>
2017-01-09 15:43:17 +0100

    Update s3-archive-image.js

--- aws-lambda-functions/s3-archive-image/s3-archive-image.js
+++ aws-lambda-functions/s3-archive-image/s3-archive-image.js
@@ -1,1 +1,60 @@
+exports.handler = (event, context, callback) => {
 
+    //
+    // Moves processed files from upload to archive directory.  Normally the
+    // last processing step. 
+    //
+
+    var AWS = require('aws-sdk');
+    var s3 = new AWS.S3({apiVersion: '2006-03-01'});
+    
+    // Retrieve parameters from export handler event
+    var bucket = event.bucket;
+    var oldFilename = event.key;
+
+    // "New file" retains same name, just the path is changed from upload to archive
+    var newFilename = event.key.replace('upload/', 'archive/');
+    
+    // Parameters for copy function
+    var archiveParams = {
+       Bucket: bucket,
+       CopySource: bucket+'/' + oldFilename,
+       Key: newFilename,
+    };
+    
+    // Parameters for delete function
+    var deleteParams = {
+       Bucket: bucket,
+       Key: oldFilename,
+    };
+    
+    // Moving requires first a copy...
+    s3.copyObject(archiveParams, function(err, data) {
+      if (err) {
+        var errorMessage =  'Error in in [s3-archive-image].\r' + 
+                                '   Error copying [' + oldFilename + '] to [' + newFilename + '] in bucket ['+bucket+'].\r' +  
+                                '   Function input ['+JSON.stringify(event, null, 2)+'].\r' +  
+                                '   Error ['+err+'].';
+              
+        console.log(errorMessage, err);
+        callback(errorMessage, null);
+      } if (data) {
+          
+        // ...followed by a delete   
+        s3.deleteObject(deleteParams, function(err, data){
+          if (err) {
+            var errorMessage =  'Error in in [s3-archive-image].\r' + 
+                                '   Error deleting [' + oldFilename +'] from bucket ['+bucket+'].\r' + 
+                                '   Function input ['+JSON.stringify(event, null, 2)+'].\r' +  
+                                '   Error ['+err+'].';
+              
+            console.log(errorMessage, err);
+            callback(errorMessage, null);
+          } else {
+            console.log('Successful archiving [' + newFilename + ']');
+            callback(null, 'Successful archiving [' + newFilename + ']');
+          }
+        });
+      }
+    });
+};
