5e2ea1f8b7239d25cad2102d41c0a8198000d4c8
Mark West <markwest1972@users.noreply.github.com>
2017-01-09 08:41:23 +0100

    Create evaluate-rekognition-labels.js

---
+++ aws-lambda-functions/evaluate-rekognition-labels.js
@@ -0,0 +1,35 @@
+exports.handler = (event, context, callback) => {
+    
+    //
+    // Evaluates labels from Rekognition and decides whether or not an  emergancy situation has been detected.
+    //
+    
+    try {
+    
+        var labels = event.Labels;
+        var key = 'Name';
+      
+        for (key in labels) {
+          if (labels.hasOwnProperty(key)) {
+            if (labels[key].Name.indexOf('Human') > -1) callback(null, Object.assign({"Alert": "true"}, event));
+            if (labels[key].Name.indexOf('People') > -1) callback(null, Object.assign({"Alert": "true"}, event));
+            if (labels[key].Name.indexOf('Person') > -1) callback(null, Object.assign({"Alert": "true"}, event));
+            if (labels[key].Name.indexOf('Male') > -1) callback(null, Object.assign({"Alert": "true"}, event));
+            if (labels[key].Name.indexOf('Female') > -1) callback(null, Object.assign({"Alert": "true"}, event));
+          }
+        }
+    
+    }catch(err){
+        
+        // Log errors
+        var errorMessage =  'Error in [evaluate-rekognition-labels].\r' + 
+                                '   Function input ['+JSON.stringify(event, null, 2)+'].\r' +  
+                                '   Error ['+err+'].';
+        log.console(errorMessage);
+        callback(errorMessage, null); // Convert to error string
+    }
+        
+    // If we get this far then no 'alert' label was found        
+    callback(null, Object.assign({"Alert": "false"}, event));
+
+};
