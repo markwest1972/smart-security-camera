a757f48ff167e7352998122b77fd3b08ae90de90
Mark West <markwest1972@users.noreply.github.com>
2017-01-15 12:04:19 +0100

    Update process-picture.sh

--- s3-upload/process-picture.sh
+++ s3-upload/process-picture.sh
@@ -4,10 +4,11 @@ echo starting s3-upload.sh
 
 echo uploading file $2
 
-node s3-upload-file.js $1  $2
+node /usr/local/bin/scripts/s3-upload/s3-upload-file.js $1  $2
 
 echo deleting file $2
 
 sudo rm $2
 
 echo completed s3-upload.sh
+
