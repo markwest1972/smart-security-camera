b25191f5875d277faabe9ac1a2f19f97c5ff476f
markwest1972 <markwest1972@gmail.com>
2017-06-19 21:40:33 +0200

    add java

---
+++ aws-lambda-functions/java/rekognition-evaluate-labels/RekognitionEvaluateLabelsHandler.java
@@ -0,0 +1,33 @@
+package no.markawest.smartsecuritycamera;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+/**
+ * Evaluates the labels returned from AWS Rekognition and sets an alert flag
+ * accordingly.
+ *
+ * Part of the Smart Security Camera project.
+ *
+ * @author mark.west
+ */
+public class RekognitionEvaluateLabelsHandler implements RequestHandler<Parameters, Parameters> {
+
+    private final List<String> alertLabels = Arrays.asList("Human", "People", "Person", "Male", "Female", "Apparel", "Clothing", "Costume", "Portrait", "Pedestrian", "Child", "Kid", "Beard", "Hair", "Face");
+
+    @Override
+    public Parameters handleRequest(Parameters parameters, Context context) {
+
+        context.getLogger().log("Input Function [" + context.getFunctionName() + "], Parameters [" + parameters + "]");
+
+        parameters.setSendAlert( parameters.getRekognitionLabels().keySet().stream().anyMatch(k -> alertLabels.contains(k.toString())) );
+
+        context.getLogger().log("Output Function [" + context.getFunctionName() + "], Parameters [" + parameters + "]");
+
+        return parameters;
+    }
+
+}
--- aws-lambda-functions/java/rekognition-evaluate-labels/readme.md
+++ aws-lambda-functions/java/rekognition-evaluate-labels/readme.md
@@ -4,18 +4,17 @@ This directory contains an AWS Lamdba Definition that evaluates whether or not t
 
 ## Contents
 
-1. **evaluate-rekognition-labels.js** - source code.
+1. **RekognitionEvaluateLabelsHandler.java** - source code.
+2. **pom.xml** - Maven pom file.
 
 ## How to use
 
 ### IAM Role
 
-Using the [AWS IAM Console](https://aws.amazon.com/console/) create an IAM Role containing the "AWSLambdaBasicExecutionRole" permission. 
+Using the [AWS IAM Console](https://aws.amazon.com/console/) create an IAM Role containing the "AWSLambdaBasicExecutionRole" permission.
 
 ### Upload to AWS
 
-Using the [AWS Lambda Console](https://aws.amazon.com/lambda), create a new Lambda Function called *evaluate-rekognition-labels* and copy the code from evaluate-rekognition-labels.js directly into the inline code editor.
+Using the [Eclipse AWS Plugin](https://aws.amazon.com/lambda), create a new Lambda Function called *rekognition-evaluate-labels-java* using the Java source and pom file..
 
 Ensure that the function uses your newly created IAM Role.
-
-Tip: Use the "blank function" blueprint and skip the "configure triggers" prompt when creating the function.
